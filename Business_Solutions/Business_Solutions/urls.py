35d423336559ee8ebd40d6637e7622b5561e0ee1
Tanvir Mahmud <tanvirmahmudfahim1313@gmail.com>
2024-04-09 23:16:35 +0600

    fixing

--- Business_Solutions/Business_Solutions/urls.py
+++ Business_Solutions/Business_Solutions/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('admin-panel/', include('app.urls'))
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
