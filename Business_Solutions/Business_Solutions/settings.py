a2c4288b11e9ab6040d8849fa381a50f29d47ab0
Tanvir Mahmud <tanvirmahmudfahim1313@gmail.com>
2024-04-10 13:37:01 +0600

    fixing bug

--- Business_Solutions/Business_Solutions/settings.py
+++ Business_Solutions/Business_Solutions/settings.py
@@ -80,12 +80,12 @@ WSGI_APPLICATION = 'Business_Solutions.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # DATABASES = {
@@ -99,16 +99,16 @@ DATABASES = {
 #     }
 # }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'railway',
-#         'USER': 'postgres',
-#         'PASSWORD': 'uARwQzoffgSNxcLdYZaIvkNqswhPLZjU',
-#         'HOST': 'roundhouse.proxy.rlwy.net',
-#         'PORT': '23075',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'uARwQzoffgSNxcLdYZaIvkNqswhPLZjU',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '23075',
+    }
+}
 
 
 
