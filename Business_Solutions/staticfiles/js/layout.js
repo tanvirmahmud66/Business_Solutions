b2135a7961e18702c77f63a1b2eb272de9e04d97
Tanvir Mahmud <tanvirmahmudfahim1313@gmail.com>
2024-04-10 15:52:27 +0600

    collectstatic

---
+++ Business_Solutions/staticfiles/js/layout.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var currentUrl = window.location.pathname;
+    console.log(currentUrl)
+    var navItems = document.querySelectorAll('.navItem');
+
+
+    navItems.forEach(function(navItem) {
+        var link = navItem.querySelector('a');
+        var href = link.getAttribute('href');
+        console.log("href",href)
+        console.log("current_url",currentUrl.startsWith(href))
+        if (currentUrl === href || currentUrl.startsWith(href)) {
+            navItem.classList.add('selected');
+        }else{
+            navItem.classList.remove('selected');
+        }
+    });
+});
